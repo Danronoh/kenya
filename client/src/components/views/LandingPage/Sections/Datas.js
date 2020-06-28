f5849396013b30b2e772568ab8906d5f304fc376
ronoh <danronohkip@gmail.com>
2020-06-28 18:33:49 +0300

    initial

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,96 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$250 to $279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$280 to $299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $300",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "General"
+    },
+    {
+        "_id": 2,
+        "name": "Cereals"
+    },
+    {
+        "_id": 3,
+        "name": "Pulses"
+    },
+    {
+        "_id": 4,
+        "name": "Oils and oilseeds"
+    },
+    {
+        "_id": 5,
+        "name": "Pasture"
+    },
+    {
+        "_id": 6,
+        "name": "Starches"
+    },
+    {
+        "_id": 7,
+        "name": "Poultry"
+    },
+    {
+        "_id": 8,
+        "name": "Rubber"
+    },
+    {
+        "_id": 9,
+        "name": "Livestock"
+    },
+    {
+        "_id": 10,
+        "name": "Fibres"
+    },
+    {
+        "_id": 11,
+        "name": "Spices and condiments"
+    },
+    {
+        "_id": 12,
+        "name": "Fruits"
+    },
+    {
+        "_id": 13,
+        "name": "Vegetables"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,139 @@
+import React, { useState } from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import FileUpload from '../../utils/FileUpload'
+import Axios from 'axios';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const Continents = [
+    { key: 1, value: "General" },
+    { key: 2, value: "Cereals" },
+    { key: 3, value: "Pulses" },
+    { key: 4, value: "Oils and oilseeds" },
+    { key: 5, value: "Pasture" },
+    { key: 6, value: "Starches" },
+    { key: 7, value: "Poultry" },
+    { key: 8, value: "Rubber" },
+    { key: 9, value: "Livestock" },
+    { key: 10, value: "Fibres" },
+    { key: 11, value: "Spices and condiments" },
+    { key: 12, value: "Fruits" },
+    { key: 13, value: "Vegatables" }
+]
+
+function UploadProductPage(props) {
+
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
+
+    const [Images, setImages] = useState([])
+
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
+    }
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
+    }
+
+    const onContinentsSelectChange = (event) => {
+        setContinentValue(event.currentTarget.value)
+    }
+
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+
+        if (!TitleValue || !DescriptionValue || !PriceValue ||
+            !ContinentValue || !Images) {
+            return alert('fill all the fields first!')
+        }
+
+        const variables = {
+            writer: props.user.userData._id,
+            title: TitleValue,
+            description: DescriptionValue,
+            price: PriceValue,
+            images: Images,
+            continents: ContinentValue,
+        }
+
+        Axios.post('/api/product/uploadProduct', variables)
+            .then(response => {
+                if (response.data.success) {
+                    alert('Product Successfully Uploaded')
+                    props.history.push('/')
+                } else {
+                    alert('Failed to upload Product')
+                }
+            })
+
+    }
+
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}> Upload  Product</Title>
+            </div>
+
+
+            <Form onSubmit={onSubmit} >
+
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
+
+                <br />
+                <br />
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={TitleValue}
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={DescriptionValue}
+                />
+                <br />
+                <br />
+                <label>Price($)</label>
+                <Input
+                    onChange={onPriceChange}
+                    value={PriceValue}
+                    type="number"
+                />
+                <br /><br />
+                <select onChange={onContinentsSelectChange} value={ContinentValue}>
+                    {Continents.map(item => (
+                        <option key={item.key} value={item.key}>{item.value} </option>
+                    ))}
+                </select>
+                <br />
+                <br />
+
+                <Button
+                    onClick={onSubmit}
+                >
+                    Submit
+                </Button>
+
+            </Form>
+
+        </div>
+    )
+}
+
+export default UploadProductPage
